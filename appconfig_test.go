e7cd2db348e6e97b02c315584abca4bb54265d2e
Brian Ketelsen <mail@bjk.fyi>
2020-02-20 13:50:27 -0500

    start with http client

--- appconfig_test.go
+++ appconfig_test.go
@@ -1,6 +1,7 @@
 package appconfig_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/Azure/appconfig-go"
@@ -8,9 +9,15 @@ import (
 
 func TestNew(t *testing.T) {
 
-	cfg := appconfig.New("Endpoint=https://test.azconfig.io;Id=xZqB-l0-s0:uN7lw/eiMARw026xtjY7;Secret=RBWq9w8DZkcvDcsXUPrLi+2ua1zdPBkLtFq6sipHMSU=")
-	if cfg.Endpoint != "https://test.azconfig.io" {
-		t.Errorf("Expected %s, got %s", "https://test.azconfig.io", cfg.Endpoint)
+	client := appconfig.New("Endpoint=https://test.azconfig.io;Id=xZqB-l0-s0:uN7lw/eiMARw026xtjY7;Secret=RBWq9w8DZkcvDcsXUPrLi+2ua1zdPBkLtFq6sipHMSU=")
+	if client.Endpoint != "https://test.azconfig.io" {
+		t.Errorf("Expected %s, got %s", "https://test.azconfig.io", client.Endpoint)
 	}
 
+}
+
+func TestUsage(t *testing.T) {
+	client := appconfig.New("Endpoint=https://test.azconfig.io;Id=xZqB-l0-s0:uN7lw/eiMARw026xtjY7;Secret=RBWq9w8DZkcvDcsXUPrLi+2ua1zdPBkLtFq6sipHMSU=")
+	keys := client.GetAllKeys()
+	fmt.Println(keys)
 }
